0bcd004b403c1b3d81166b0b7d410fa29550dd91
AndroidVinay <jambhalevinay@gmail.com>
2015-11-19 18:53:25 +0530

    added login activity

--- app/src/main/java/com/example/vppl_10132/scrollabletoolbar/floating_action_button_behaviour/ScrollAwareFABBehavior.java
+++ app/src/main/java/com/example/vppl_10132/scrollabletoolbar/floating_action_button_behaviour/ScrollAwareFABBehavior.java
@@ -4,19 +4,21 @@ import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
 import android.view.View;
 
-import java.util.jar.Attributes;
 
 /**
  * Created by VPPL-10132 on 11/12/2015.
  */
 public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
 
+    Context context;
 
-    public ScrollAwareFABBehavior(Context context, Attributes attrs) {
+    public ScrollAwareFABBehavior(Context context, AttributeSet attrs) {
         super();
     }
+
 
     @Override
     public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
@@ -31,9 +33,9 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
     public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
         if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
-            child.hide();
-        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
             child.show();
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            child.hide();
         }
     }
 }
